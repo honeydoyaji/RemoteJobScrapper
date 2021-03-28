b7327af39f0d5474a018fbde09a42f91e401415d
Seoul <jinnylucky@naver.com>
2021-03-29 01:50:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ db = {}
 
 @app.route("/")
 def home():
-    return render_template("home.html", template_folder='../templates')
+    return render_template("home.html", template_folder="templates")
+
 @app.route("/report")
 def report():
     word = request.args.get('word')
